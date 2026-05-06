7a0c6e99a3bb0144cc1acded1ad1331429fd1fcd
twisterph <phinojosaiturra@byupathway.edu>
2026-05-05 20:04:20 -0700

    w1 Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0;
+
+            Console.WriteLine("Guess My Number Game");
+
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string userInput = Console.ReadLine();
+
+                guess = int.Parse(userInput);
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.WriteLine($"Number of guesses: {guessCount}");
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
