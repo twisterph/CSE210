26ba95c01735ff61477a4a7729bb0f5a6c234a83
Pablo Hinojosa <phinojosaiturra@byupathway.edu>
2026-05-26 20:00:28 -0700

    Complete w04 online ordering program

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("123 Lake View Dr", "Lake Havasu City", "AZ", "USA");
+        Customer customer1 = new Customer("Pablo Hinojosa", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Wireless Mouse", "WM100", 15.99, 2));
+        order1.AddProduct(new Product("USB-C Cable", "UC200", 8.50, 3));
+        order1.AddProduct(new Product("Laptop Stand", "LS300", 32.99, 1));
+
+        Address address2 = new Address("45 Los Alerces", "Santiago", "RM", "Chile");
+        Customer customer2 = new Customer("Carlos Rivera", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Bluetooth Keyboard", "BK400", 24.99, 1));
+        order2.AddProduct(new Product("Webcam", "WC500", 39.99, 1));
+        order2.AddProduct(new Product("Headset", "HS600", 29.99, 2));
+
+        DisplayOrder(order1);
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.GetTotalCost():F2}");
+        Console.WriteLine("------------------------------");
     }
 }
