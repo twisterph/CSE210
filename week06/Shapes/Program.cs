47b4209d35c9bd19ff1af3a093692aabe07bccbf
Pablo Hinojosa <phinojosaiturra@byupathway.edu>
2026-06-03 18:42:10 -0700

    w06 3

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(
+                $"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
