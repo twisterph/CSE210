c402ad67c2a9ebb9968612ba3ded57177a17fcff
Pablo Hinojosa <phinojosaiturra@byupathway.edu>
2026-06-07 23:12:45 -0700

    final activity

---
+++ week07/ExerciseTracking/Swimming.cs
@@ -0,0 +1,24 @@
+public class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(string date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return _laps * 50.0 / 1000;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetMinutes() / GetDistance();
+    }
+}
